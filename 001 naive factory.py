aced402d7f997a59e1766c24927501a0aafd6164
iubizi <106417012+iubizi@users.noreply.github.com>
2022-06-11 03:10:57 -0400

    Add files via upload

---
+++ 001 naive factory.py
@@ -0,0 +1,15 @@
+class apple(object):
+    # apple
+    def __repr__(self):
+        return 'apple factory'
+
+class bnana(object):
+    # bnana
+    def __repr__(self):
+        return 'bnana factory'
+
+fruit_1 = apple()
+fruit_2 = bnana()
+
+print(fruit_1)
+print(fruit_2)
